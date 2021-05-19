5c167dd79f692c034b9b1b18b93aa7d290531f57
Osvaldo Hernández <angel.hernandezc16@tectijuana.edu.mx>
2021-05-19 11:48:29 -0700

    update urls

--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,4 @@
+from django import urls
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
@@ -7,5 +8,6 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
